bb2030798e061c5f915dba27b775f798e5e5aea6
DmitriyChebruchan <chebruchan2707@gmail.com>
2025-12-05 20:38:52 +0200

    refacoting

--- js/preview.js
+++ js/preview.js
@@ -68,12 +68,12 @@ function updatePreview() {
         
         <p>Dear Sirs</p>
         
-        <p>vessel: ${highlightPlaceholder(vesselName)}</p>
+        <p>Ship: ${highlightPlaceholder(vesselName)}</p>
         <p>Voyage: ${highlightPlaceholder(voyage)}</p>
         <p>Cargo: ${highlightPlaceholder(cargo)}</p>
         <p>Bill of lading: ${highlightPlaceholder(billOfLading)}</p>
         
-        <p>The above cargo was shipped on the above vessel by ${highlightPlaceholder(shipper)} and consigned to ${highlightPlaceholder(consignee)} for delivery at the port of ${highlightPlaceholder(dischargePortDisplay)} but the bill of lading has not arrived and we, ${highlightPlaceholder(requestingParty)}, hereby request you to deliver the said cargo to X ${highlightPlaceholder(deliveryParty)} or to such party as you believe to be or to represent X or to be acting on behalf of X at ${highlightPlaceholder(deliveryPlace)} without production of the original bill of lading.</p>
+        <p>The above cargo was shipped on the above ship by ${highlightPlaceholder(shipper)} and consigned to ${highlightPlaceholder(consignee)} for delivery at the port of ${highlightPlaceholder(dischargePortDisplay)} but the bill of lading has not arrived and we, ${highlightPlaceholder(requestingParty)}, hereby request you to deliver the said cargo to X ${highlightPlaceholder(deliveryParty)} or to such party as you believe to be or to represent X or to be acting on behalf of X at ${highlightPlaceholder(deliveryPlace)} without production of the original bill of lading.</p>
         
         <p>In consideration of your complying with our above request, we hereby agree as follows :-</p>
         
@@ -81,9 +81,9 @@ function updatePreview() {
         
         <p>2. In the event of any proceedings being commenced against you or any of your servants or agents in connection with the delivery of the cargo as aforesaid, to provide you or them on demand with sufficient funds to defend the same.</p>
         
-        <p>3. If, in connection with the delivery of the cargo as aforesaid, the vessel, or any other vessel or property in the same or associated ownership, management or control, should be arrested or detained or should the arrest or detention thereof be threatened, or should there be any interference in the use or trading of the vessel (whether by virtue of a caveat being entered on the vessel's registry or otherwise howsoever), to provide on demand such bail or other security as may be required to prevent such arrest or detention or to secure the release of such vessel or property or to remove such interference and to indemnify you in respect of any liability, loss, damage or expense caused by such arrest or detention or threatened arrest or detention or such interference, whether or not such arrest or detention or threatened arrest or detention or such interference may be justified.</p>
+        <p>3. If, in connection with the delivery of the cargo as aforesaid, the shep, or any other ship or property in the same or associated ownership, management or control, should be arrested or detained or should the arrest or detention thereof be threatened, or should there be any interference in the use or trading of the ship (whether by virtue of a caveat being entered on the ship's registry or otherwise howsoever), to provide on demand such bail or other security as may be required to prevent such arrest or detention or to secure the release of such ship or property or to remove such interference and to indemnify you in respect of any liability, loss, damage or expense caused by such arrest or detention or threatened arrest or detention or such interference, whether or not such arrest or detention or threatened arrest or detention or such interference may be justified.</p>
         
-        <p>4. If the place at which we have asked you to make delivery is a bulk liquid or gas terminal or facility, or another vessel, lighter or barge, then delivery to such terminal, facility, vessel, lighter or barge shall be deemed to be delivery to the party to whom we have requested you to make such delivery.</p>
+        <p>4. If the place at which we have asked you to make delivery is a bulk liquid or gas terminal or facility, or another ship, lighter or barge, then delivery to such terminal, facility, ship, lighter or barge shall be deemed to be delivery to the party to whom we have requested you to make such delivery.</p>
         
         <p>5. As soon as all original bills of lading for the above cargo shall have come into our possession, to deliver the same to you, or otherwise to cause all original bills of lading to be delivered to you, whereupon our liability hereunder shall cease.</p>
         
